980ad4c02abe6df17d81b2761523a333179005b9
gopal.tidke <gopaltidke3@gmail.com>
2022-05-23 10:23:56 +0530

    added class

---
+++ tte/src/lastproject/Gopal.java
@@ -0,0 +1,13 @@
+package lastproject;
+
+public class Gopal {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b =10, c=50 ,d=66;
+		System.out.println(a+""+b+""+c+""+d);
+	}
+	
+	
+	
+}
